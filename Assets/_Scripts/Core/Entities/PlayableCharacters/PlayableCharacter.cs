a6f9ba566898dc05c9a9210de6959fd64d9720ea
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2022-08-21 16:40:35 +0300

    Drone and Human scripts created

--- Assets/_Scripts/Core/Entities/PlayableCharacters/PlayableCharacter.cs
+++ Assets/_Scripts/Core/Entities/PlayableCharacters/PlayableCharacter.cs
@@ -2,17 +2,11 @@ using UnityEngine;
 
 namespace Core.Entities.PlayableCharacters
 {
-    public class PlayableCharacter : Entity
+    public abstract class PlayableCharacter : Entity
     {
         [SerializeField] private Input _input;
         [SerializeField] private bool _isActive = false;
         public bool IsActive => _isActive;
-
-        private void FixedUpdate()
-        {
-            if (_isActive)
-                Move(_input.GetMovementInput(this));
-        }
 
         public void Activate() => _isActive = true;
         public void Deactivate() => _isActive = false;
